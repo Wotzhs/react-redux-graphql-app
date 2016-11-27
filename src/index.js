327654d6e4a6119e503e4d64a24b7d469780783d
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-27 22:38:09 +0800

    remove unneeded css depenedency

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import '../vendor/skeleton/normalize.css';
 import '../vendor/skeleton/skeleton.css';
 import './index.css';
 
