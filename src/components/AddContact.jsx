8f1a44374d45dba40b8b5975023f6d9b95a99940
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-28 00:11:41 +0800

    added styling for add contact component & a default avatar

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const styles = {
+	default: {
+		paddingTop: 25
+	},
+	minHeight100px: {
+		minHeight: 100
+	},
+	clickableAvatar: {
+		height: 195,
+		cursor: 'pointer'
+	},
+	centered: {
+		textAlign: 'center',
+	}
+}
+
 class AddContact extends React.Component{
 
 	createContact(event){
@@ -8,14 +24,42 @@ class AddContact extends React.Component{
 
 	render(){
 		return(
-			<form onSubmit={this.createContact}>
-				<input type="text" placeholder="Name"/>
-				<input type="text" placeholder="Phone"/>
-				<input type="email" placeholder="Email"/>
-				<input type="text" placeholder="Organization"/>
-				<textarea cols="30" rows="10" placeholder="comments"></textarea>
-				<input type="submit" value="Save"/>
-				<input type="button" value="Cancel"/>
+			<form onSubmit={ this.createContact } style={ styles.default } >
+				<div className="row">
+					<div className="two columns">&ensp;</div>
+					<div className="two columns" style={ styles.centered } >
+						<img className="u-max-full-width" src="/default_avatar.jpg" alt="default avatar" style={ styles.clickableAvatar } />
+					</div>
+					<div className="six columns">
+						<input className="u-full-width" type="text" placeholder="Name"/>
+						<input className="u-full-width" type="text" placeholder="Phone"/>
+						<input className="u-full-width" type="email" placeholder="Email"/>
+						<input className="u-full-width" type="text" placeholder="Organization"/>
+					</div>
+					
+				</div>
+
+				<div className="row">
+					<div className="two columns">&ensp;</div>
+					<div className="eight columns">
+						<textarea 
+							className="u-full-width" 
+							placeholder="comments" 
+							style={ styles.minHeight100px }>
+						</textarea>
+					</div>
+					<div className="two columns"></div>
+				</div>
+				
+				<div className="row">
+					<div className="two columns">&ensp;</div>
+					<div className="four columns">
+						<input className="u-full-width button-primary" type="submit" value="Save"/>
+					</div>
+					<div className="four columns">
+						<input className="u-full-width" type="button" value="Cancel"/>
+					</div>
+				</div>				
 			</form>
 		)
 	}
