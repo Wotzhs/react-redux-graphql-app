a32d9aac257d46bef3f7b00a7de301f9220507b0
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-27 19:56:03 +0800

    changed file extension for emmet to work

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LandingPage extends React.Component{
+	render(){
+		return(
+			<div>
+				<h1>Intro & Description</h1>
+				<br/>
+				<h2>How it works section</h2>
+				<br/>
+				<h1>Footer</h1>
+			</div>
+		)
+	}
+}
+
+export default LandingPage;
