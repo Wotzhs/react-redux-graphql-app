5d0eba1a70f237025ea96c456bc8b2acf9de9c6b
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-27 23:18:58 +0800

    updated footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ class Footer extends React.Component{
 	render(){
 		return(
 			<div style={styles.rightAlign}>&copy; 2016 by
-				<a href="#" style={styles.profile}><i> WOTZHS</i></a>
+				<a href="#" style={styles.profile}> TzeHsiung Wong</a>
 			</div>
 		)
 	}
