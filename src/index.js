2a7a24d461da68c9c5b25c344461c53adceaee82
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-28 23:39:17 +0800

    added skeleton default framework css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+import '../vendor/skeleton/normalize.css';
 import '../vendor/skeleton/skeleton.css';
 import './index.css';
 
