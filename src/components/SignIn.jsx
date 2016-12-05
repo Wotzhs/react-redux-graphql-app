dfc99f57c306e9478c7487d73e807d3ce72c8882
Wong Tze Hsiung <wotzhs@gmail.com>
2016-12-05 22:15:00 +0800

    setup of bindings for submited form for sign in validation

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -12,23 +12,25 @@ class SignUp extends React.Component{
 
 	createAccount(event){
 		event.preventDefault();
-		browserHistory.push('/home');
+		console.log(this.email.value);
+		console.log(this.password.value);
+		// browserHistory.push('/home');
 	}
 
 	render(){
 		return(
-			<form style={styles.container} onSubmit={this.createAccount}>
+			<form style={styles.container} onSubmit={ (e) => this.createAccount(e) }>
 				<div className="row">
 					<div className="three columns">&ensp;</div>
 					<div className="six columns">
-						<input className="u-full-width" type="email" placeholder="Email"/>
+						<input ref={ (input) => this.email = input } className="u-full-width" type="email" placeholder="Email"/>
 					</div>
 					<div className="three columns"></div>
 				</div>
 				<div className="row">
 					<div className="three columns">&ensp;</div>
 					<div className="six columns">
-						<input className="u-full-width" type="password" placeholder="Password"/>
+						<input ref={ (input) => this.password = input } className="u-full-width" type="password" placeholder="Password"/>
 					</div>
 					<div className="three columns"></div>
 				</div>				
