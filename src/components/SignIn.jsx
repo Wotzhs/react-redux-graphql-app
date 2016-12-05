2ea647af967227a6c01b3fcad2faad9fd7e4492d
Wong Tze Hsiung <wotzhs@gmail.com>
2016-12-05 22:00:22 +0800

    added redirection from signin page to homepage

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 
 const styles = {
 	container: {
@@ -11,6 +12,7 @@ class SignUp extends React.Component{
 
 	createAccount(event){
 		event.preventDefault();
+		browserHistory.push('/home');
 	}
 
 	render(){
