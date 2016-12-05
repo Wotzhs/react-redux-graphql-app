9d46adb4316c879d556c91f738f7172d7c7f4a69
Wong Tze Hsiung <wotzhs@gmail.com>
2016-12-05 23:27:17 +0800

    added logic to clear clear button when input is null & clear search input when clear is clicked

--- src/components/ContactSearch.jsx
+++ src/components/ContactSearch.jsx
@@ -6,20 +6,51 @@ const styles = {
 		padding: 15,
 		fontFamily: 'fontAwesome'
 	},
-	clearButton: {
+	show: {
 		paddingRight: 10,
 		marginTop: -42,
 		position: 'relative',
 		cursor: 'pointer'
+	},
+	hide: {
+		display: 'none'
 	}
 }
 
 class ContactSearch extends React.Component{
+
+	constructor(){
+		super();
+		this.state = {
+			show: false
+		}
+	}
+
+	search(event){
+		this.searchInput.value.length ? this.setState({show: true}) : this.setState({show: false})
+	};
+
+	clearInput(){
+		this.searchInput.value = null;
+		this.setState({show:false});
+	}
+
 	render(){
 		return(
 			<div style={ styles.padding15px }>
-				<input type="text" placeholder="&#xf002;  Search..." className="u-full-width"/>
-				<Icon name="times" className="u-pull-right" style={ styles.clearButton }/>
+				<input 
+					type="text"
+					placeholder="&#xf002;  Search..."
+					className="u-full-width"
+					onChange={ (e) => this.search(e) }
+					ref={ (input) => this.searchInput = input }
+				/>
+				<Icon
+					name="times" 
+					className="u-pull-right" 
+					style={ this.state.show ? styles.show : styles.hide  }
+					onClick={ (e) => this.clearInput(e)}
+				/>
 			</div>
 		)
 	}
