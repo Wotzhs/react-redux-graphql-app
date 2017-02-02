41dba1a7f54c4f1106cf9a83f717dbf72f8e8a5e
Wong Tze Hsiung <wotzhs@gmail.com>
2017-02-02 20:06:23 +0800

    redux update nested state working

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -3,7 +3,13 @@ function contacts( state = {}, action ) {
 	switch( action.type ) {
 		case 'ADD_CONTACT' :
 			const timestamp = Date.now();
-			return {...state, [`contact-${timestamp}`]: action.contactDetails } ;
+			return { ...state, allContacts: { ...state.allContacts, [`contact-${timestamp}`]: action.contactDetails } };
+		case 'EDIT_CONTACT' :
+			return { ...state, [ action.contactId ] : action.contactUpdatedDetails };
+		case 'DELETE_CONTACT' :
+			return { ...state };
+		case 'LOAD_CONTACT' :
+			return { ...state };
 		default:
 			return state;
 	}
