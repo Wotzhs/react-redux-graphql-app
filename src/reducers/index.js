6da206eebe7d770013016d4feb6515cda7673b77
Wong Tze Hsiung <wotzhs@gmail.com>
2017-07-02 16:02:21 +0800

    update index reducers to include new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,14 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import contacts from './contacts';
+import currentUser from './currentUser';
+import auth from './auth';
 
-const rootReducer = combineReducers( { contacts, routing: routerReducer } );
+const rootReducer = combineReducers( { 
+	contacts,
+	currentUser,
+	auth,
+	routing: routerReducer 
+} );
 
 export default rootReducer;
