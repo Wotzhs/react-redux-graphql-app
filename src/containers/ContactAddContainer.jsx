a611b0b6562721828b32c91d3e962fdfa8d44776
Sean Wong <wotzhs@gmail.com>
2017-07-05 00:07:23 +0800

    redirect to home upon successful additions

--- src/containers/ContactAddContainer.jsx
+++ src/containers/ContactAddContainer.jsx
@@ -1,12 +1,16 @@
 import { connect } from 'react-redux';
 import { addContact, addContactSuccess, addContactError } from '../actions/contacts';
+import { browserHistory } from 'react-router';
 import ContactAdd from '../components/ContactAdd';
 
 const mapDispatchToProps = dispatch => {
 	return {
 		addContact: contactDetails => {
 			dispatch( addContact( contactDetails ) )
-				.then( () => dispatch( addContactSuccess( 'Successfully added' ) ) )
+				.then( () => { 
+					dispatch( addContactSuccess( 'Successfully added' ) );
+					browserHistory.push('/home')
+				} )
 				.catch( err => dispatch( addContactError( err ) ) );
 		}
 	}
