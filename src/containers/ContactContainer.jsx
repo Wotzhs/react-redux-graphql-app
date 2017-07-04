dd642147116fb3812328c83add3594c747b91edc
Sean Wong <wotzhs@gmail.com>
2017-07-05 00:45:17 +0800

    merge with deprecated contact add container

--- src/containers/ContactContainer.jsx
+++ src/containers/ContactContainer.jsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
-import { loadContacts, loadContactsSuccess } from '../actions/contacts';
+import { loadContacts, loadContactsSuccess, addContact, addContactSuccess, addContactError } from '../actions/contacts';
+import { browserHistory } from 'react-router';
 import ContactIndex from '../components/ContactIndex';
 
 const mapStateToProps = state => {
@@ -16,6 +17,14 @@ const mapDispatchToProps = dispatch => {
 					dispatch( loadContactsSuccess( res.payload.data.contacts ) ) 
 				} )
 				.catch( err => console.log( err))
+		},
+		addContact: contactDetails => {
+			dispatch( addContact( contactDetails ) )
+				.then( () => { 
+					dispatch( addContactSuccess( 'Successfully added' ) );
+					browserHistory.push('/home')
+				} )
+				.catch( err => dispatch( addContactError( err ) ) );
 		}
 	}
 }
