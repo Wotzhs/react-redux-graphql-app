9efd96ce53f2536601172efd5374e0c5c1718ea2
Sean Wong <wotzhs@gmail.com>
2017-07-05 00:10:11 +0800

    add contact view compoennt to display contact detai;s

---
+++ src/components/ContactView.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Icon } from 'react-fa';
+import { Link } from 'react-router';
+
+const styles = {
+	editButton: {
+		paddingLeft: '3%',
+		paddingRight: '3%',
+		width: '50%'
+	},
+	deleteButton: {
+		paddingLeft: '3%',
+		paddingRight: '3%',
+		width: '50%',
+		color: 'red'
+	},
+	minHeight240px: {
+		minHeight: 240
+	},
+	show: {
+		padding: '0 15px 15px 15px',
+		backgroundColor: 'white',
+		minHeight: 380,
+		zIndex: 2
+	},
+	showChild:{
+		display: 'block'
+	},
+	hide: {
+		display: 'none'
+	},
+	selectable: {
+		cursor: 'pointer',
+		color: '#1EAEDB'
+	}
+}
+
+class ContactDetails extends React.Component{
+
+	render(){
+		
+		let activeContact = { name: ''};
+
+		if ( this.props.hasOwnProperty('list') ) {
+			activeContact = this.props.list.find( ( contact ) => { return contact._id === this.props.routeParams.id } );
+		}
+
+		return(
+			<div>
+				<div className="row">
+					<h5>
+						<Link to={ '/home'} ><Icon name="mail-reply"/></Link>
+						&ensp;{ activeContact === undefined ? null : activeContact.name }
+					</h5>	
+				</div>
+				
+				<div className="row" style={ styles.minHeight240px }>
+					<div className="eight columns">
+						<div className="row">
+							<div className="six columns">
+								<label>Phone</label>
+								<p>{ activeContact === undefined ? null : activeContact.tel }</p>
+							</div>
+							<div className="six columns">
+								<label>Email</label>
+								<a href="mailto:wotzhs@gmail.com" target="_top">{ activeContact === undefined ? null : activeContact.email }</a>
+							</div>
+						</div>
+						
+						<label>Organization</label>
+						<p>{ activeContact === undefined ? null : activeContact.organisation }</p>
+						<label>Comment</label>
+						<p>{ activeContact === undefined ? null : activeContact.comment }</p>
+					</div>
+				</div>
+
+				<br/>
+
+				<div className="row">
+					<button 
+						className="button" 
+						style={ styles.editButton }
+						onClick={(e)=> this.handleClick(e) }
+					>
+						<Icon name="pencil"/> Edit
+					</button>
+					<button className="button" style={ styles.deleteButton }><Icon name="trash"/> Delete</button>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default ContactDetails;
